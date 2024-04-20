5460b92e61a293c5330b228530c6925656ebc05b
Dante Teder <danteteder@gmail.com>
2024-04-20 14:46:21 +0300

    some changes

--- src/layout/MainContent/MainContent.tsx
+++ src/layout/MainContent/MainContent.tsx
@@ -1,35 +1,50 @@
 import { FC } from "react";
+// import { useWindowSize } from "hooks";
+import { Typography, Button } from 'antd';
 
-import { useWindowSize } from "hooks";
-
-type MainContentProps = {
-  children?: React.ReactNode;
-};
+const { Title, Paragraph } = Typography;
 
 const styles = {
   content: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "flex-start",
     marginTop: "100px",
     padding: "50px",
     overflow: "auto"
   },
-  contentMobile: {
+  leftHalf: {
+    width: "50%",
     display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    marginTop: "100px",
-    padding: "30px 0",
-    overflow: "hidden"
+    flexDirection: "column",
+    alignItems: "flex-start", // align to the left
+    justifyContent: "center"
+  },
+  rightHalf: {
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end", // align to the right
+    justifyContent: "center"
   }
 } as const;
 
-const MainContent: FC<MainContentProps> = ({ children }) => {
-  const { isTablet } = useWindowSize();
+const MainContent: FC = () => {
 
-
-  return <div style={isTablet ? styles.contentMobile : styles.content}>{children}</div>;
+  return (
+    <div>
+      <div style={styles.leftHalf}>
+        <Title level={2}>Title 1</Title>
+        <Paragraph>Text underneath Title 1</Paragraph>
+        <Title level={2}>Title 2</Title>
+        <Paragraph>Text underneath Title 2</Paragraph>
+      </div>
+      <div style={styles.rightHalf}>
+        <Button type="primary" size="large">Create</Button>
+        <Button type="primary" size="large">Retrieve</Button>
+      </div>
+    </div>
+  );
 };
 
 export default MainContent;
